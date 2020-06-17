711fa68ea04c1094ad5b9949a842628816590c73
Jessica Pereira <jcpereira070@gmail.com>
2020-06-17 23:27:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.upcase
+  else phrase != phrase.upcase
     'HUH?! SPEAK UP, SONNY!'
   end
 end
