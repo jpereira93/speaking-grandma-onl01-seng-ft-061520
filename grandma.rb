fd23a3367662a2de077397cf2665f642d6d71f5a
Jessica Pereira <jcpereira070@gmail.com>
2020-06-17 23:17:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ def speak_to_grandma(phrase)
   
   if phrase != phrase.upcase puts "HUH?! SPEAK UP, SONNY!"
 
-  elsif phrase == phrase.upcase 
-    puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!" 
+  elseif phrase == "I LOVE YOU GRANDMA!" 
     puts "I LOVE YOU TOO PUMPKIN!"
+    else phrase == phrase.upcase 
+    puts "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
